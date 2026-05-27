15532f40ea321c1f3b3ff186c6e3c901e8fe9f1f
Evgeny Usorov <eusorov@gmail.com>
2026-05-27 15:45:19 +0200

    added EntityGraph to avoid n+1 problem

--- app/src/main/java/com/bowatt/instagramm/api/repositories/ImageRepository.java
+++ app/src/main/java/com/bowatt/instagramm/api/repositories/ImageRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = "tags")
+    Optional<Image> findById(Long id);
 
     @EntityGraph(attributePaths = "tags")
     Page<Image> findAllByOrderByCreatedAtDesc(Pageable pageable);
